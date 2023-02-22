b52dafd2836f4cba9f34eb0c4cc1db640ceab127
Alvin Kannalath <akannalath@gmail.com>
2023-02-21 20:03:05 -0500

    Comments Done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
+// Constants for the frame dimensions
 const FRAME_HEIGHT = 550;
 const FRAME_WIDTH = 550; 
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
+//This creates an svg for the 1st visual
 const FRAME1 = d3.select("#vis-1") 
                   .append("svg") 
                     .attr("height", FRAME_HEIGHT)   
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame"); 
 
-
+//This function creates the graph with the various points on the graph through the
+// imported csv
 function build_scatter_plot() {
 	d3.csv("data/scatter-data.csv").then((data) => {
 
@@ -18,7 +21,7 @@ function build_scatter_plot() {
     // Define scale functions that maps our data values 
     // (domain) to pixel values (range)
     const X_SCALE = d3.scaleLinear() 
-                      .domain([0, 10]) // add some padding  
+                      .domain([0, 10]) //x add some padding  
                       .range([0, VIS_WIDTH]);
 
     // Define scale functions that maps our data values 
@@ -98,7 +101,7 @@ function build_scatter_plot() {
 		let y = Math.round(Y_SCALE.invert(d3.select(circle).attr('cy') - MARGINS.top));
 
 
-
+		//This list the most recent coordinate clicked
 		let coordinate = `(${x}, ${y})`;
 		document.getElementById("lastClicked").innerHTML = coordinate;
 		console.log(x);
@@ -122,14 +125,14 @@ function build_scatter_plot() {
           .attr("transform", "translate(" + MARGINS.left + 
                 "," + (VIS_HEIGHT + MARGINS.top) + ")") 
           .call(d3.axisBottom(X_SCALE).ticks(10)) 
-            .attr("font-size", '20px');
+            .attr("font-size", '10px');
 
     // adds y axis labels
     FRAME1.append("g") 
           .attr("transform", "translate(" + MARGINS.left + 
                 "," + MARGINS.top + ")")
           .call(d3.axisLeft(Y_SCALE).ticks(10)) 
-            .attr("font-size", '20px');  
+            .attr("font-size", '10px');  
 
     });
 }
@@ -138,55 +141,67 @@ build_scatter_plot();
 
 ////////////////////////////////////////////////////////////////
 
+//This creates an svg for the bar graph
 const FRAME2 = d3.select("#vis-2") 
                   .append("svg") 
                     .attr("height", FRAME_HEIGHT)   
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame"); 
 
+//Creates a function to structure the bar graph
 function build_bar_plot() {
 	d3.csv("data/bar-data.csv").then((data) => {
 
-	  const xScale = d3.scaleBand()
+		// Define scale functions that maps our data values 
+		// provides some space between each line on the bar graph
+	  const X_SCALE2 = d3.scaleBand()
 	  	.domain(data.map(function(d) { return d.category; }))
 	  	.range([0, VIS_WIDTH]).padding(0.1);
 
-	  const yScale = d3.scaleLinear()
+	  // Define scale functions that maps our data values 
+	  const Y_SCALE2 = d3.scaleLinear()
 	  	.domain([0, 100])
 	  	.range([VIS_HEIGHT, 0]);
 
+	  //Allows the bar graph to be spaced over alligned with the above graph
 		const g = FRAME2.append("g")
 	             .attr("transform", "translate(" + MARGINS.top + "," + MARGINS.left + ")");
 
+	  //Put the x-axis on the bottom of the graph
 	  g.append("g")
 	   .attr("transform", "translate(0," + VIS_HEIGHT + ")")
-	   .call(d3.axisBottom(xScale));
+	   .call(d3.axisBottom(X_SCALE2));
 
+		//Put the y-axis on the side of the graph
 	  g.append("g")
-	   .call(d3.axisLeft(yScale).ticks(10))
+	   .call(d3.axisLeft(Y_SCALE2).ticks(10))
 	   		.attr("font-size", "10px");
 
+	  // When you hover over the bar line, it turns pink
  		function mouseOver() {
     	d3.select(this).attr("fill", "lightpink");
 		}
 
+		// When you leave the line it goes back to blue
 		function mouseOut() {
     	d3.select(this).attr("fill", "lightblue");
 		}
 
+		// Creates the svg element with the axes and bar lines filled in with blue
 		g.selectAll()
 	   .data(data)
 	   .enter()
 	   .append("rect")
-		   .attr("x", function(d) { return xScale(d.category); })
-		   .attr("y", function(d) { return yScale(d.amount); })
-		   .attr("width", xScale.bandwidth())
-		   .attr("height", function(d) { return VIS_HEIGHT - yScale(d.amount); })
+		   .attr("x", function(d) { return X_SCALE2(d.category); })
+		   .attr("y", function(d) { return Y_SCALE2(d.amount); })
+		   .attr("width", X_SCALE2.bandwidth())
+		   .attr("height", function(d) { return VIS_HEIGHT - Y_SCALE2(d.amount); })
 		   .attr("fill", "lightblue")
-		   .attr("class", "point")
 		   .on("mouseover", mouseOver)
 		   .on("mouseout", mouseOut);
 
+		// Creates the tooltip for the visual so when you hover over the line, the values
+		// pop up
 		const TOOLTIP = d3.select("#vis-2")
 						.append("div")
 							.attr("class", "tooltip")
@@ -201,7 +216,7 @@ function build_bar_plot() {
 		function handleMousemove(event, d) {
 			TOOLTIP.html("Category: " + d.category + "<br>Value: " + d.amount)
 					.style("left", (event.pageX + 10) + "px")
-					.style("top", (event.pageY - 50) + "px")
+					.style("top", (event.pageY - 50) + "px");
 		}
 
 		//mouseleave
@@ -209,14 +224,14 @@ function build_bar_plot() {
 			TOOLTIP.style("opacity", 0);
 		}
 
-		// add event listener to points
-		g.selectAll(".point")
+		// add event listener to the bar lines
+		g.selectAll(".tooltip")
 				.on("mouseover", handleMouseover)
 				.on("mousemove", handleMousemove)
-				.on("mouseleave", handleMouseleave);
-
+				.on("mouseleave", handleMouseleave)
   });
 }
 
+//Builds the bar chart
 build_bar_plot()
 
